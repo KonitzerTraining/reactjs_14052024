66a17097489c44fd6d7b0f0648cae47fb9968b7d
M. Konitzer <mk@konitzer-training.de>
2024-05-16 11:30:17 +0200

    routing

--- crm-kolibri/src/App.tsx
+++ crm-kolibri/src/App.tsx
@@ -1,23 +1,37 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { StartPage } from './pages/start/StartPage';
+import { HelpIndex } from './pages/help/HelpIndex';
+
+import { AboutPage } from './pages/about/AboutPage';
+import { CustomerIndex } from './pages/customers/CustomerIndex';
+import { MainNavigation } from './MainNavigation';
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <StartPage></StartPage>,
+      errorElement: <h1>Page Not Found</h1>
+    },
+    {
+      path: "/dashboard",
+      element: <CustomerIndex></CustomerIndex>,
+    },
+    {
+      path: "/about",
+      element: <AboutPage></AboutPage>,
+    },
+    {
+      path: "/help",
+      element: <HelpIndex></HelpIndex>,
     },
   ]);
 
-
-
   return (
     <>
-
-      <StartPage></StartPage>
+      <MainNavigation></MainNavigation>
       <RouterProvider router={router} />
-
     </>
   )
 }
---
+++ crm-kolibri/src/MainNavigation.tsx
@@ -0,0 +1,12 @@
+export function MainNavigation() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Start</a></li>
+                <li><a href="/dashboard">Dashboard</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/help">Help</a></li>
+            </ul>
+        </nav>
+    );
+}
