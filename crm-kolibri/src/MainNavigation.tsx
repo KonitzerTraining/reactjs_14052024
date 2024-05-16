3d3d17357280557cd23eda83387830df9233f878
M. Konitzer <mk@konitzer-training.de>
2024-05-16 12:15:30 +0200

    nav

--- crm-kolibri/src/MainNavigation.tsx
+++ crm-kolibri/src/MainNavigation.tsx
@@ -6,26 +6,26 @@ export function MainNavigation() {
 
 <KolNav
 	_label="Navigation"
+    _orientation="horizontal"
 	_links="[
     {
-      '_href': 'startseite',
+      '_href': '/',
       '_icons': 'codicon codicon-home',
-      '_label': 'Start',
-      '_children': []
+      '_label': 'Start'
     },
     {
-      '_href': 'dashboard',
+      '_href': '/dashboard',
       '_icons': 'codicon codicon-home',
       '_label': 'Dashboard'
     },
     {
-        '_href': 'about',
+        '_href': '/about',
         '_icons': 'codicon codicon-home',
         '_label': 'About'
       },
       
     {
-        '_href': 'help',
+        '_href': '/help',
         '_icons': 'codicon codicon-home',
         '_label': 'Help'
       }
