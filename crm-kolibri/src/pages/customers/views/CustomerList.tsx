65e8d3ac8bd14e52d2e23576c1a541e6f066769c
M. Konitzer <mk@konitzer-training.de>
2024-05-16 14:30:24 +0200

    CustomerList

--- crm-kolibri/src/pages/customers/views/CustomerList.tsx
+++ crm-kolibri/src/pages/customers/views/CustomerList.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { Customer } from "../../../model/Customer";
+import { customerService } from "../../../services/customer.service";
+
 export function CustomerList() {
+
+
+    // Einsatz eines Hooks
+    const [customers, setCustomers ] = useState<Customer[]>([]);
+    const loadCustomers = () => {
+        customerService.getAll().then((c) => {
+            console.log(c);
+            setCustomers(c);
+        });        
+    }
+
+    useEffect(() => {
+        loadCustomers();
+    }, []);
+
     return (
         <>
             <h2>List</h2>
+            {JSON.stringify(customers)}
         </>
     )
 }
