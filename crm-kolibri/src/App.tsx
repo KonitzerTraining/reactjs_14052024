a37fc1231881434a2371db8e10fd87dbcbf2c48e
M. Konitzer <mk@konitzer-training.de>
2024-05-16 10:32:48 +0200

    router

--- crm-kolibri/src/App.tsx
+++ crm-kolibri/src/App.tsx
@@ -1,13 +1,22 @@
-import { KolSpin } from '@public-ui/react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { StartPage } from './pages/start/StartPage';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
 
 
   return (
     <>
 
       <StartPage></StartPage>
+      <RouterProvider router={router} />
 
     </>
   )
