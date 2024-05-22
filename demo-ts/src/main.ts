613b340edc90db2eda3de7644965852fe7e8de91
M. Konitzer <mk@konitzer-training.de>
2024-05-22 15:41:31 +0200

    TS

--- demo-ts/src/main.ts
+++ demo-ts/src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+let num1 = 200; // implizite Typisierung
+num1 = '200';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Zustand wie in JavaScript
+let num2: any = 200;
+num2 = '200';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// string, boolen
+
+let surname: string = 'Taylor';
+
+let names: string[] = ['a', 3453];
+
+let id: string | number = 3;
+
+let role: 'admin' | 'editor' | null = null;
+console.log(role, typeof role); 
