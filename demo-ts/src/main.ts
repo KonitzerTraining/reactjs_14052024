15e6eac0e14735c09325f6a7e7647fc141d25179
M. Konitzer <mk@konitzer-training.de>
2024-05-22 15:49:08 +0200

    ts

--- demo-ts/src/main.ts
+++ demo-ts/src/main.ts
@@ -12,17 +12,20 @@ let surname: string = 'Taylor';
 
 let names: string[] = ['a', 3453];
 
+// Auswahl unter verschiedenen Typen
 let id: string | number = 3;
 
+// Werte als Typen
 let role: 'admin' | 'editor' | null = null;
 console.log(role, typeof role); 
 
-
+// für komplexe Objekte, hat keine Entsprechung in JavaScript
 interface Customer {
   id: number
   name: string
 }
 
+// customers ist nur ein Array, keine Instanz von Customer vorhanden
 let customers: Customer[] = [
   {
     id: 3,
